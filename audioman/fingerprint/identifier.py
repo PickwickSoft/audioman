04890b353376e259c6f372685da2207c6c1a2700
Stefan Garlonta <stefan.garlonta@gmail.com>
2021-08-13 13:50:44 +0000

    Implement audio file identifier to get ID of file

---
+++ audioman/fingerprint/identifier.py
@@ -0,0 +1,32 @@
+import acoustid
+from audioman.errors.exceptions import *
+
+class AudioFileIdentifier():
+
+    def __init__(self, file: str):
+        # The Api Key is from
+        self.__API_KEY = 'f3wnNGn8-AQ'
+        self.__file = file
+        self.__id = None
+
+    def identify(self) -> str:
+        try:
+            results = acoustid.match(self.__API_KEY, self.__file)
+        except acoustid.NoBackendError:
+            raise ChromaprintNotFoundException("Chromaprint library/tool not found")
+        except acoustid.FingerprintGenerationError:
+            raise FingerprintGenerationError("Fingerprint could not be calculated")
+        except acoustid.WebServiceError as exc:
+            raise WebServiceError("Web service request failed:", exc.message)
+        result = next(results)
+        self.__id = result[1]
+        return self.__id
+
+    def get_id(self) -> str:
+        if self.__id == None:
+            raise FileNotIdentifiedException("Run 'identify()' first")
+        return self.__id
+
+    def set_new_file(self, file: str):
+        self.__file = file
+        self.__id = None
