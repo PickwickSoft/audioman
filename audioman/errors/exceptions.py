e2a437b24e254b3d2f4274d514399fefe9fd6849
Stefan Garlonta <stefan.garlonta@gmail.com>
2021-08-13 13:41:27 +0000

    Implement Exceptions

---
+++ audioman/errors/exceptions.py
@@ -0,0 +1,14 @@
+class ChromaprintNotFoundException(Exception):
+    pass
+
+
+class FingerprintGenerationError(Exception):
+    pass
+
+
+class WebServiceError(Exception):
+    pass
+
+
+class FileNotIdentifiedException(Exception):
+    pass
