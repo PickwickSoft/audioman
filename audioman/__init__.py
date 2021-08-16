4d078e1600c6f1e983ac1fc2f7e476dca0a4f0b9
Stefan Garlonta <stefan.garlonta@gmail.com>
2021-08-16 14:54:45 +0000

    Refactorings

--- audioman/__init__.py
+++ audioman/__init__.py
@@ -3,5 +3,6 @@ __app__ = "audioman"
 __contact__ = "https://github.com/ThePickwickClub/audioman"
 includes = ["artists", "releases", "media", "artist-credits", "isrcs", "work-level-rels",
             "annotation", "aliases", "tags", "ratings", "area-rels",
-            "artist-rels", "label-rels", "place-rels", "event-rels", "recording-rels", "release-rels",
-            "release-group-rels", "series-rels", "url-rels", "work-rels", "instrument-rels"]
+            "artist-rels", "label-rels", "place-rels", "event-rels", "recording-rels",
+            "release-rels", "release-group-rels", "series-rels", "url-rels", "work-rels",
+            "instrument-rels"]
