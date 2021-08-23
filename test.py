3f88531b10fd74a1460b00b9ada472078a9991f7
Stefan Garlonta <stefan.garlonta@gmail.com>
2021-08-23 12:23:16 +0000

    Adapt test

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
-#!/usr/bin/python3
 from audioman.fingerprint.identifier import AudioFileIdentifier
 from audioman.locator.audio_file_locator import AudioFileLocator
 from audioman.lookup.metadata_lookup import MetadataLookup
 from audioman.parser.metadata_parser import MetadataParser
 from audioman.metadata.tagger import AudioTagger
+from audioman.files.structure import Structure, FolderStructure, FileNameStructure
+from audioman.files.restructure import FileRestructurer
+
 files = AudioFileLocator.locate_files(".", "test")
 print(files)
+
+if len(files) == 0:
+    exit(0)
+
 identifier = AudioFileIdentifier(files[0])
 id = identifier.identify()
 print('http://musicbrainz.org/recording/%s' % id)
+
 data = MetadataLookup.lookup(str(id))
-# print(data)
 metadata = MetadataParser(data).parse_metadata()
+metadata.album = "The Best of"
 print(metadata.artist, metadata.rating, metadata.title, sep=", ")
+
 tagger = AudioTagger.get_tagger(files[0])
 tagger.tag(metadata)
 tagger.save()
+
+FileRestructurer("./", Structure(FolderStructure.ARTIST_ALBUM,
+                 FileNameStructure.ARTIST_TITLE)).move(files[0], metadata)
