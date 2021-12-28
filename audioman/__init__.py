43e9627c9bb1c4bc6a382e13cb2f53ac81b51885
Stefan Garlonta <stefan.garlonta@gmail.com>
2021-12-28 17:12:22 +0100

    Reduce musicbrainz includes list

--- audioman/__init__.py
+++ audioman/__init__.py
@@ -1,8 +1,11 @@
 __version__ = "1.0"
 __app__ = "audioman"
 __contact__ = "https://github.com/ThePickwickClub/audioman"
+"""
 includes = ["artists", "releases", "media", "artist-credits", "isrcs", "work-level-rels",
             "annotation", "aliases", "tags", "ratings", "area-rels",
             "artist-rels", "label-rels", "place-rels", "event-rels", "recording-rels",
             "release-rels", "release-group-rels", "series-rels", "url-rels", "work-rels",
             "instrument-rels"]
+"""
+includes = ["artists", "releases"]
